fd5a5cbe084f9390d4528c8048c1679716080907
bruh man <Darian321@outlook.com>
2023-03-29 16:53:49 -0400

    more things that take 4 seconds to write

--- CTC/src/main/java/com/example/ctc/BLL/Model/TaxBracket.java
+++ CTC/src/main/java/com/example/ctc/BLL/Model/TaxBracket.java
@@ -1,4 +1,41 @@
 package com.example.ctc.BLL.Model;
 
 public class TaxBracket {
+    private float min;
+    private float max;
+    private float rate;
+
+    public TaxBracket() {
+
+    }
+
+    public TaxBracket(float min, float max, float rate) {
+        this.min = min;
+        this.max = max;
+        this.rate = rate;
+    }
+
+    public float getMin() {
+        return min;
+    }
+
+    public void setMin(float min) {
+        this.min = min;
+    }
+
+    public float getMax() {
+        return max;
+    }
+
+    public void setMax(float max) {
+        this.max = max;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
 }
--- GTC/src/main/java/com/example/gtc/BLL/Model/Transaction.java
+++ GTC/src/main/java/com/example/gtc/BLL/Model/Transaction.java
@@ -1,4 +1,41 @@
 package com.example.gtc.BLL.Model;
 
 public class Transaction {
+
+    private String date;
+    private float grossRevenue;
+    private float tax;
+
+    public Transaction() {
+    }
+
+    public Transaction(String date, float grossRevenue, float tax) {
+        this.date = date;
+        this.grossRevenue = grossRevenue;
+        this.tax = tax;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getGrossRevenue() {
+        return grossRevenue;
+    }
+
+    public void setGrossRevenue(float grossRevenue) {
+        this.grossRevenue = grossRevenue;
+    }
+
+    public float getTax() {
+        return tax;
+    }
+
+    public void setTax(float tax) {
+        this.tax = tax;
+    }
 }
