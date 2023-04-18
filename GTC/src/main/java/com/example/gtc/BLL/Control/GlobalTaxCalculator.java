5299f0fa461438e2ffec0eb377748d07bddec8a7
Toriku097 <ttramirez097@gmail.com>
2023-04-17 20:32:58 -0400

    QuebecTaxController getTax without the TaxBracket class 'pien'

--- GTC/src/main/java/com/example/gtc/BLL/Control/GlobalTaxCalculator.java
+++ GTC/src/main/java/com/example/gtc/BLL/Control/GlobalTaxCalculator.java
@@ -1,4 +1,30 @@
 package com.example.gtc.BLL.Control;
 
 public class GlobalTaxCalculator {
+    double getTax(double amount) {
+        double total = 0;
+        //49 020 ou moins
+        if (amount <= 46295 ){
+            total = amount * .15;
+        }
+        //entre 49 020 et 98 040
+        else if (amount > 46295 && amount <= 92580) {
+            total = 46295 * .15;
+            total = total + ((amount - 46295) * .205);
+        }
+        //entre 98 040 et 151 978
+        else if (amount > 92580 && amount <= 112655) {
+            total = 46295 * .15;
+            total = total + (92580 * .205);
+            total = total + ((amount - 92580) * .26);
+        }
+        //plus de 216 511
+        else if (amount > 112655) {
+            total = 46295 * .15;
+            total = total + (92580 * .205);
+            total = total + (112655 * .26);
+            total = total + ((amount - 112655) * 32);
+        } else
+            return total;
+    }
 }
