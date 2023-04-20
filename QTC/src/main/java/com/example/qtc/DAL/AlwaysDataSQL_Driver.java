c2ee73507463d0b99d22db23d6f5020c11e7358a
bruh man <Darian321@outlook.com>
2023-04-19 21:44:59 -0400

    surprise mysql connection for QTC

---
+++ QTC/src/main/java/com/example/qtc/DAL/AlwaysDataSQL_Driver.java
@@ -0,0 +1,40 @@
+package com.example.qtc.DAL;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AlwaysDataSQL_Driver {
+
+    private Connection conn;
+
+    public AlwaysDataSQL_Driver() {
+        this.conn = connect();
+    }
+
+    public Connection connect() {
+        MysqlDataSource datasource = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
+        datasource.setUser("306943");
+        datasource.setPassword("bigcappy45");
+        datasource.setServerName("mysql-wokemoralistgaming25.alwaysdata.net");
+        datasource.setPort(3306);
+        datasource.setDatabaseName("wokemoralistgaming25_projetdbsql");
+        Connection conn;
+
+        try {
+            conn = datasource.getConnection();
+            return conn;
+        } catch (SQLException ex) {
+            Logger.getLogger(AlwaysDataSQL_DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+}
