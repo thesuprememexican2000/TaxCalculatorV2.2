d247c2b7aad914cfbdf4e2eaac9cb6a23fd9f9b1
Toriku097 <ttramirez097@gmail.com>
2023-04-19 20:24:36 -0400

    InMemory_DAO: constructeur pour la liste de brackets CTController: nouvelle méthode qui utilise les taxbrackets

--- CTC/src/main/java/com/example/ctc/BLL/Control/CanadaTaxController.java
+++ CTC/src/main/java/com/example/ctc/BLL/Control/CanadaTaxController.java
@@ -1,39 +1,60 @@
 package com.example.ctc.BLL.Control;
 
+import com.example.ctc.BLL.Model.TaxBracket;
+import com.example.ctc.DAL.InMemory_DAO;
+
+import java.util.List;
+
 public class CanadaTaxController {
-    //retourne l'imposition sur le salaire donné
+    InMemory_DAO dao;
+    List<TaxBracket> TaxBrackets;
+
+    public CanadaTaxController() {
+        dao = new InMemory_DAO();
+        TaxBrackets = dao.getTaxBrackets();
+    }
+
     double getTax(double amount) {
-        double total = 0;
-        //49 020 ou moins
-        if (amount <= 49020 ){
-            total = amount * .15;
+
+        for (TaxBracket taxBracket : TaxBrackets) {
+            if (taxBracket.getMin() <= amount && amount < taxBracket.getMax()) {
+                return amount * taxBracket.getRate();
+            }
         }
-        //entre 49 020 et 98 040
-        else if (amount > 49020 && amount <= 98040) {
-            total = 49020 * .15;
-            total = total + ((amount - 49020) * .205);
-        }
-        //entre 98 040 et 151 978
-        else if (amount > 98040 && amount <= 151978) {
-            total = 49020 * .15;
-            total = total + (98040 * .205);
-            total = total + ((amount - 98040) * .26);
-        }
-        //entre 151 978 et 216 511
-        else if (amount > 151978 && amount <= 216511) {
-            total = 49020 * .15;
-            total = total + (98040 * .205);
-            total = total + (151978 * .26);
-            total = total + ((amount - 151978) * .29);
-        }
-        //plus de 216 511
-        else if (amount > 216511) {
-            total = 49020 * .15;
-            total = total + (98040 * .205);
-            total = total + (151978 * .26);
-            total = total + (216511 * .29);
-            total = total + ((amount - 216511) * 32);
-        }
-        return total;
+        return 0;
     }
 }
+//    double getTax(double amount) {
+//        double total = 0;
+//        //49 020 ou moins
+//        if (amount <= 49020 ){
+//            total = amount * .15;
+//        }
+//        //entre 49 020 et 98 040
+//        else if (amount > 49020 && amount <= 98040) {
+//            total = 49020 * .15;
+//            total = total + ((amount - 49020) * .205);
+//        }
+//        //entre 98 040 et 151 978
+//        else if (amount > 98040 && amount <= 151978) {
+//            total = 49020 * .15;
+//            total = total + (98040 * .205);
+//            total = total + ((amount - 98040) * .26);
+//        }
+//        //entre 151 978 et 216 511
+//        else if (amount > 151978 && amount <= 216511) {
+//            total = 49020 * .15;
+//            total = total + (98040 * .205);
+//            total = total + (151978 * .26);
+//            total = total + ((amount - 151978) * .29);
+//        }
+//        //plus de 216 511
+//        else if (amount > 216511) {
+//            total = 49020 * .15;
+//            total = total + (98040 * .205);
+//            total = total + (151978 * .26);
+//            total = total + (216511 * .29);
+//            total = total + ((amount - 216511) * 32);
+//        }
+//        return total;
+//    }
--- CTC/src/main/java/com/example/ctc/DAL/InMemory_DAO.java
+++ CTC/src/main/java/com/example/ctc/DAL/InMemory_DAO.java
@@ -1,4 +1,32 @@
 package com.example.ctc.DAL;
 
+import com.example.ctc.BLL.Model.TaxBracket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemory_DAO {
+    List<TaxBracket> list;
+
+    public InMemory_DAO() {
+        list = new ArrayList<>();
+
+        TaxBracket bracket = new TaxBracket(0,49020,.15f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(49020,98040,.205f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(98040,151978,.26f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(151978,216511,.29f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(216511,Float.MAX_VALUE,.33f);
+        list.add(bracket);
+    }
+    public List<TaxBracket> getTaxBrackets() {
+        return list;
+    }
 }
