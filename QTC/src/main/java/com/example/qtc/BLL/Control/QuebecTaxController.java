216b6e38082847aaef3bac7d36776458e1fd29c5
Toriku097 <ttramirez097@gmail.com>
2023-04-19 20:17:21 -0400

    InMemory_DAO: constructeur pour la liste de brackets QTController: nouvelle méthode qui utilise les taxbrackets

--- QTC/src/main/java/com/example/qtc/BLL/Control/QuebecTaxController.java
+++ QTC/src/main/java/com/example/qtc/BLL/Control/QuebecTaxController.java
@@ -1,30 +1,51 @@
 package com.example.qtc.BLL.Control;
 
+import com.example.qtc.BLL.Model.TaxBracket;
+import com.example.qtc.DAL.InMemory_DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuebecTaxController {
+    InMemory_DAO dao;
+    List<TaxBracket> TaxBrackets;
+
+    public QuebecTaxController() {
+        dao = new InMemory_DAO();
+        TaxBrackets = dao.getTaxBrackets();
+    }
+
     double getTax(double amount) {
-        double total = 0;
-        //49 020 ou moins
-        if (amount <= 46295 ){
-            total = amount * .15;
+
+        for (TaxBracket taxBracket : TaxBrackets) {
+            if (taxBracket.getMin() <= amount && amount < taxBracket.getMax()) {
+                return amount * taxBracket.getRate();
+            }
         }
-        //entre 49 020 et 98 040
-        else if (amount > 46295 && amount <= 92580) {
-            total = 46295 * .15;
-            total = total + ((amount - 46295) * .205);
-        }
-        //entre 98 040 et 151 978
-        else if (amount > 92580 && amount <= 112655) {
-            total = 46295 * .15;
-            total = total + (92580 * .205);
-            total = total + ((amount - 92580) * .26);
-        }
-        //plus de 216 511
-        else if (amount > 112655) {
-            total = 46295 * .15;
-            total = total + (92580 * .205);
-            total = total + (112655 * .26);
-            total = total + ((amount - 112655) * 32);
-        }
-        return total;
+        return 0;
     }
 }
+
+//    double getTax(double amount) {
+//        double total = 0;
+//        if (amount <= 46295 ){
+//            total = amount * .15;
+//        }
+//        //entre 49 020 et 98 040
+//        else if (amount > 46295 && amount <= 92580) {
+//            total = 46295 * .15;
+//            total = total + ((amount - 46295) * .205);
+//        }
+//        //entre 98 040 et 151 978
+//        else if (amount > 92580 && amount <= 112655) {
+//            total = 46295 * .15;
+//            total = total + (92580 * .205);
+//            total = total + ((amount - 92580) * .26);
+//        }
+//        //plus de 216 511
+//        else if (amount > 112655) {
+//            total = 46295 * .15;
+//            total = total + (92580 * .205);
+//            total = total + (112655 * .26);
+//            total = total + ((amount - 112655) * 32);
+//        }
--- QTC/src/main/java/com/example/qtc/DAL/InMemory_DAO.java
+++ QTC/src/main/java/com/example/qtc/DAL/InMemory_DAO.java
@@ -1,4 +1,29 @@
 package com.example.qtc.DAL;
 
+import com.example.qtc.BLL.Model.TaxBracket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemory_DAO {
+    List<TaxBracket> list;
+
+    public InMemory_DAO() {
+        list = new ArrayList<>();
+
+        TaxBracket bracket = new TaxBracket(0,46295,.15f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(46295,92580,.2f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(92580,112655,.24f);
+        list.add(bracket);
+
+        bracket = new TaxBracket(112655,Float.MAX_VALUE,.2575f);
+        list.add(bracket);
+    }
+    public List<TaxBracket> getTaxBrackets() {
+        return list;
+    }
 }
